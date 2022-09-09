f5eaae76e1bebe0d62532ced05596cb3fbed4c92
hasibulalamrahi <hasibul.alam@brainstation-23.com>
2022-09-09 19:02:50 +0600

    file configues changed

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+require("dotenv").config();
 //Importing Database Config
-const db = require('./config/database')
+require("./config/database");
 
-//importing Routes 
+//importing middlewares
+const notFound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/error");
 
-const subjectsRouter = require('./routes/subjectRoutes')
-const userAdminRoutes = require('./routes/createAdminRoutes')
-
-db.authenticate()
-                .then(()=> console.log('Databse Connected Successfully...'))
-                .catch((err)=>console.log('Error: ',err))
-
+//importing Routes
+const subjectsRouter = require("./routes/subjectRoutes");
+const userAdminRoutes = require("./routes/createAdminRoutes");
 
 const app = express();
+app.use(express.json());
+
+//declaring cors
+app.use(cors());
 
 //json parser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//morgan and helmet packages
+app.use(morgan("dev"));
+app.use(helmet());
+
 //Routes
-app.use('/api/v1/subjects',subjectsRouter)
-app.use('/api/v1/admins',userAdminRoutes)
+app.use("/api/v1/subjects", subjectsRouter);
+app.use("/api/v1/admins", userAdminRoutes);
 
+//declaring all the middlewares
+app.use(notFound);
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
-const Sequelize = require('sequelize')
-module.exports = new Sequelize('Quiz_App', 'postgres', 'bs23', {
-  host: 'localhost',
-  dialect:  'postgres'  ,
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("Quiz_App", "postgres", "bs23", {
+  host: "localhost",
+  dialect: "postgres",
   operatorsAliases: false,
 
-  pool:{
+  pool: {
     max: 5,
     min: 0,
     acquire: 3000,
-    idle: 10000
-  }
-})
-
+    idle: 10000,
+  },
+});
+sequelize
+  .authenticate()
+  .then(() => console.log("Databse Connected Successfully..."))
+  .catch((err) => console.log("Error: ", err));
+module.exports = sequelize;
